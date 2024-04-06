b21ef7e6c0cdb59c73664d031ce3783c05b8f1bf
godotg <jaysunxiao@gmail.com>
2024-04-06 17:41:06 +0800

    update

--- gdscript/script/08-binary.gd
+++ gdscript/script/08-binary.gd
@@ -1,9 +1,31 @@
 extends SceneTree
 
 func _init():
-    var a = 1
-    var b = 2
+    var a = 0b00000001
+    var b = 0b00000011
     var c = a + b
     print(c)
     print("Hello World!")
+    binaryNumber(c)
     quit()
+
+
+func binaryNumber(value):
+    var buffer = StreamPeerBuffer.new()
+    buffer.put_64(value);
+    buffer.seek(0)
+    print(buffer.get_8())
+    buffer.seek(1)
+    print(buffer.get_8())
+    buffer.seek(2)
+    print(buffer.get_8())
+    buffer.seek(3)
+    print(buffer.get_8())
+    buffer.seek(4)
+    print(buffer.get_8())
+    buffer.seek(5)
+    print(buffer.get_8())
+    buffer.seek(6)
+    print(buffer.get_8())
+    buffer.seek(7)
+    print(buffer.get_8())

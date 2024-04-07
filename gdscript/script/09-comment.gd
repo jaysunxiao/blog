1187d47041190a576901aa60fea7d1dececc55c6
godotg <jaysunxiao@gmail.com>
2024-04-07 12:02:21 +0800

    update

---
+++ gdscript/script/09-comment.gd
@@ -0,0 +1,18 @@
+extends SceneTree
+
+# 单行注释1
+func _init():  # 单行注释2
+# 单行注释3
+    var a = 1
+    var b = 2
+    var c = a + b
+    print(c)
+    print("Hello World!")
+    quit()
+# 单行注释4
+
+
+"""
+这是一个多行注释
+这里可以写很多内容
+"""

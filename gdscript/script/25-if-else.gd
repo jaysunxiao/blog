b0f353dc8d708e640b6e8a0596613adfbbc650d4
godotg <jaysunxiao@gmail.com>
2024-04-14 11:22:48 +0800

    update

--- gdscript/script/25-if-else.gd
+++ gdscript/script/25-if-else.gd
@@ -1,10 +1,26 @@
 extends SceneTree
 
 func _init():
-    var x = 5
-    if (x > 3):
-        print("x is greater than 3")
-        print("hello")
+    # 定义一个变量，表示道具类型
+    var item_type = "potion"
+
+    # 使用match语句模拟switch语句
+    match item_type:
+        "weapon":
+            print("这是一个武器。")
+        "armor":
+            print("这是一件防具。")
+        "potion":
+            print("这是一瓶药水。")
+        _:
+            print("未知道具类型。")
+
+    if item_type == "weapon":
+        print("这是一个武器。")
+    elif item_type == "armor":
+        print("这是一件防具。")
+    elif item_type == "potion":
+        print("这是一瓶药水。")
     else:
-        print("x is not greater than 3")
+        print("未知道具类型。")
     quit()

89989ce8152b6509da5ac82cec43f395a1060703
godotg <jaysunxiao@gmail.com>
2024-04-14 11:12:41 +0800

    update

---
+++ gdscript/script/25-if-else.gd
@@ -0,0 +1,10 @@
+extends SceneTree
+
+func _init():
+    var x = 5
+    if (x > 3):
+        print("x is greater than 3")
+        print("hello")
+    else:
+        print("x is not greater than 3")
+    quit()
